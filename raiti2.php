300cac37474756ea40df4bc5b76d901db54fe566
dimepsy <dimestoner@gmail.com>
2017-02-10 10:16:11 +0545

    This is a php file
    nothing

---
+++ raiti2.php
@@ -0,0 +1,47 @@
+<?php
+$count=0;
+	include("configure.php");
+	$postid=$_GET['id'];
+	//$date=$_GET['Date'];
+	//echo $postid;
+	
+	$sql="SELECT  * FROM upload2 WHERE id='$postid'";
+	
+	
+	
+	$retval= mysqli_query($connect,$sql);
+	if(! $retval)
+	{
+		die('Could not get data : ' .mysqli_error());	
+	}
+	
+	$row = mysqli_fetch_array($retval);
+	
+	/*for($i=0;$row = mysql_fetch_array($retval, MYSQL_ASSOC); $i++)
+	{
+		
+		$b[$i]= $row['name'];
+		$c[$i] = $row['description'];
+		
+		$count++;	
+	}*/
+	
+?>
+
+
+<div id="other">
+    
+			<!-- Articles -->
+			<div class="row">
+				<article class="col-xs-12">
+					<h2><?php echo $row['title'];?> </h2>
+					
+                    
+					<p><?php echo $row['description'];?><br>
+						
+                    </p>
+					<b>Published Date:</b> <?php echo $row['Date'];?><br>
+					<b>Last Edited By:</b> <?php echo $row['Last_Edited_By'];?><br>
+				</article>
+			</div>
+		</div>
